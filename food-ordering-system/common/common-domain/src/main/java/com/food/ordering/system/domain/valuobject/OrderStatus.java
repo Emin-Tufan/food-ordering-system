9e55f930fd91c1386308996a20b433adad4a435a
Emin <eminttufan@gmail.com>
2023-11-07 17:02:31 +0300

    updated valueObject && created Dto classes

--- food-ordering-system/common/common-domain/src/main/java/com/food/ordering/system/domain/valuobject/OrderStatus.java
+++ food-ordering-system/common/common-domain/src/main/java/com/food/ordering/system/domain/valuobject/OrderStatus.java
@@ -1,5 +1,5 @@
 package com.food.ordering.system.domain.valuobject;
 
 public enum OrderStatus {
-    PENDING, CANCELED, PAID, APPROVED, CANCELLED
+    PENDING, CANCELLING, PAID, APPROVED, CANCELLED
 }
