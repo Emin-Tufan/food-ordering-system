0e325dd055d2d615fc8a5efc83bf04a500a19278
Emin <eminttufan@gmail.com>
2023-11-08 14:22:18 +0300

    update ports

--- food-ordering-system/order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/order/service/domain/ports/input/message/listener/restaurantapproval/RestaurantApprovalMessageListener.java
+++ food-ordering-system/order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/order/service/domain/ports/input/message/listener/restaurantapproval/RestaurantApprovalMessageListener.java
@@ -1,2 +1,9 @@
-package com.food.ordering.system.order.service.domain.ports.input.message.listener.restaurantapproval;public interface RestaurantApprovalMessageListener {
+package com.food.ordering.system.order.service.domain.ports.input.message.listener.restaurantapproval;
+
+import com.food.ordering.system.order.service.domain.dto.message.RestaurantApprovalResponse;
+
+public interface RestaurantApprovalMessageListener {
+    void orderApproved(RestaurantApprovalResponse restaurantApprovalResponse);
+
+    void orderRejected(RestaurantApprovalResponse restaurantApprovalResponse);
 }
--- food-ordering-system/order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/order/service/domain/ports/output/message/publisher/payment/OrderCancelledPaymentRequestPublisher.java
+++ food-ordering-system/order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/order/service/domain/ports/output/message/publisher/payment/OrderCancelledPaymentRequestPublisher.java
@@ -1,2 +1,7 @@
-package com.food.ordering.system.order.service.domain.ports.output.message.publisher.payment;public interface OrderCancelledPaymentRequestPublisher {
+package com.food.ordering.system.order.service.domain.ports.output.message.publisher.payment;
+
+import com.food.ordering.system.domain.event.publisher.DomainEventPublisher;
+import com.food.ordering.system.order.service.domain.event.OrderCancelledEvent;
+
+public interface OrderCancelledPaymentRequestPublisher extends DomainEventPublisher<OrderCancelledEvent> {
 }
