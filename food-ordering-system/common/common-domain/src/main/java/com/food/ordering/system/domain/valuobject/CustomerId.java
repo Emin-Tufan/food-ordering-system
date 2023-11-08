c462df1b7f36fbf861b88d3bde81115e8c0458af
Emin <eminttufan@gmail.com>
2023-11-08 14:26:00 +0300

    updated valueobjects

--- food-ordering-system/common/common-domain/src/main/java/com/food/ordering/system/domain/valuobject/CustomerId.java
+++ food-ordering-system/common/common-domain/src/main/java/com/food/ordering/system/domain/valuobject/CustomerId.java
@@ -2,8 +2,8 @@ package com.food.ordering.system.domain.valuobject;
 
 import java.util.UUID;
 
-public class CustomerId extends BaseId<UUID>{
-    protected CustomerId(UUID value) {
+public class CustomerId extends BaseId<UUID> {
+    public CustomerId(UUID value) {
         super(value);
     }
 }
