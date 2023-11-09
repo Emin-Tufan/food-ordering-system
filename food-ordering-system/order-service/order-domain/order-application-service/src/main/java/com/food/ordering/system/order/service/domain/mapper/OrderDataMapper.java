efa81e5114ffac6e1da3789d1fe523e7be495f08
Emin <eminttufan@gmail.com>
2023-11-09 23:34:01 +0300

    listeners implemented

--- food-ordering-system/order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/order/service/domain/mapper/OrderDataMapper.java
+++ food-ordering-system/order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/order/service/domain/mapper/OrderDataMapper.java
@@ -1,17 +1,16 @@
 package com.food.ordering.system.order.service.domain.mapper;
 
-import com.food.ordering.system.domain.valuobject.CustomerId;
-import com.food.ordering.system.domain.valuobject.Money;
-import com.food.ordering.system.domain.valuobject.ProductId;
-import com.food.ordering.system.domain.valuobject.RestaurantId;
+import com.food.ordering.system.domain.valuobject.*;
 import com.food.ordering.system.order.service.domain.dto.create.CreateOrderCommand;
 import com.food.ordering.system.order.service.domain.dto.create.CreateOrderResponse;
 import com.food.ordering.system.order.service.domain.dto.create.OrderAddress;
+import com.food.ordering.system.order.service.domain.dto.track.TrackOrderResponse;
 import com.food.ordering.system.order.service.domain.entity.Order;
 import com.food.ordering.system.order.service.domain.entity.OrderItem;
 import com.food.ordering.system.order.service.domain.entity.Product;
 import com.food.ordering.system.order.service.domain.entity.Restaurant;
 import com.food.ordering.system.order.service.domain.valueobject.StreetAddress;
+import com.food.ordering.system.order.service.domain.valueobject.TrackingId;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
@@ -46,6 +45,14 @@ public class OrderDataMapper {
                 .build();
     }
 
+    public TrackOrderResponse orderToTrackOrderResponse(Order order) {
+        return TrackOrderResponse.builder()
+                .orderTrackingId(order.getTrackingId().getValue())
+                .status(order.getOrderStatus())
+                .failureMessages(order.getFailureMessages())
+                .build();
+    }
+
     private List<OrderItem> orderItemsToOrderItemEntities(List<com.food.ordering.system.order.service.domain.dto.create.OrderItem> items) {
         return items.stream()
                 .map(orderItem -> OrderItem.builder()
@@ -55,7 +62,6 @@ public class OrderDataMapper {
                         .quantity(orderItem.getQuantity())
                         .build())
                 .collect(Collectors.toList());
-
     }
 
     private StreetAddress orderAddressToStreetAddress(OrderAddress address) {
